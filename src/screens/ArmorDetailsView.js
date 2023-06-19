7a44c0dd1bbd43785092ca4cf24fd1587c2529da
Vabre ben <naruben74@hotmail.fr>
2023-06-19 16:00:30 +0200

    testing css bs

--- src/screens/ArmorDetailsView.js
+++ src/screens/ArmorDetailsView.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Image, StyleSheet } from "react-native";
 
 const ArmorDetailsView = ({ armor, onGoBack }) => {
   return (
     <View>
+      <Image style={styles.image} source={{uri: armor.assets && armor.assets.imageMale}} />
       <Text>{armor.name}</Text>
       <Button title="Retour" onPress={onGoBack} />
     </View>
   );
 };
+const styles = StyleSheet.create({
+  image: {
+    height: 360,
+    width: 360,
+  },
+});
 
 export default ArmorDetailsView;
