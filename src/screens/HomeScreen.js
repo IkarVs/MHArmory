6841e5f3309dd24116d0e1d23d1cb3e31c541d43
Vabre ben <naruben74@hotmail.fr>
2023-06-21 11:12:03 +0200

    Fixed Button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-  Button, Pressable,
+  Button,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -18,53 +19,77 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import DisplayACatImage from '../components/Cat';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
 
 function GoogleSignIn() {
   GoogleSignin.configure({
-    webClientId: '432126369602-8fbkntp5p15lefulln4f5b5j985bs89t.apps.googleusercontent.com',
+    webClientId:
+      '432126369602-8fbkntp5p15lefulln4f5b5j985bs89t.apps.googleusercontent.com',
   });
   async function onGoogleButtonPress() {
     // Check if your device supports Google Play
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
     // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn();
+    const {idToken} = await GoogleSignin.signIn();
 
     // Create a Google credential with the token
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-      googleCredential.then((cred)=>{
-        console.log(cred);
-      }).catch((error)=>{console.log(error)})
     // Sign-in the user with the credential
     return auth().signInWithCredential(googleCredential);
   }
   return (
     <Button
       title="Google Sign-In"
-      onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+      onPress={() =>
+        onGoogleButtonPress().then(() => console.log('Signed in with Google!'))
+      }
     />
   );
 }
-export default function HomeScreen({navigation}){
+export default function HomeScreen({navigation}) {
   return (
     <View>
-      <GoogleSignIn/>
-      <Pressable style={styles.button_cat} onPress={() => navigation.navigate('Cats', {animal: 'cats'})}>
-        <Text style={styles.text}>Test Chat</Text>
-      </Pressable>
-
-      <Pressable style={styles.button_dog}  onPress={() => navigation.navigate('Cats', {animal: 'Chien'})}>
-        <Text style={styles.text}>Test Pas Chat</Text>
-      </Pressable>
+      <Test />
     </View>
   );
 }
+function Test() {
+  GoogleSignin.configure({
+    webClientId:
+      '432126369602-8fbkntp5p15lefulln4f5b5j985bs89t.apps.googleusercontent.com',
+  });
+  const SignInWithGoogleAsync = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    // Sign-in the user with the credential
+    const user_sign_in = auth().signInWithCredential(googleCredential);
+    user_sign_in
+      .then(user => {
+        console.log(user);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <View>
+      <Button title="Sign in With Google" onPress={SignInWithGoogleAsync} />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   button_cat: {
-    position: 'relative' ,
-    top : 200,
-    left:100,
+    position: 'relative',
+    top: 200,
+    left: 100,
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 12,
@@ -72,13 +97,13 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     elevation: 3,
     backgroundColor: 'black',
-    width:200,
-    height:50,
+    width: 200,
+    height: 50,
   },
   button_dog: {
-    position: 'relative' ,
-    top : 400,
-    left:100,
+    position: 'relative',
+    top: 400,
+    left: 100,
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 12,
@@ -86,8 +111,8 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     elevation: 3,
     backgroundColor: 'black',
-    width:200,
-    height:50,
+    width: 200,
+    height: 50,
   },
   text: {
     fontSize: 16,
